53afddc32d86b7137be80785d72b525242e8ed72
Enrico Mischorr <enrico@mischorr.de>
2019-08-21 16:37:20 +0200

    allow IFrame

--- lib/deploy_dashboard_web/router.ex
+++ lib/deploy_dashboard_web/router.ex
@@ -8,6 +8,7 @@ defmodule DeployDashboardWeb.Router do
     plug Phoenix.LiveView.Flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug DeployDashboardWeb.Plug.AllowIframe
   end
 
   pipeline :api do
